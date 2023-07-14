abb99659fe1043bdf8032d30b0beecaccd25ac37
Jtljac <jtljac2@gmail.com>
2023-07-15 00:53:41 +0100

    Testing

--- examples/test-project/main.cpp
+++ examples/test-project/main.cpp
@@ -1,3 +1,17 @@
 //
 // Created by jacob on 27/06/23.
 //
+
+
+#include <iostream>
+
+#include <SDL.h>
+#include <util/logger.h>
+#include <rendering/vulkan/vulkan-renderer.h>
+
+int main() {
+    VulkanRenderer renderer;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    DatLog::init();
+    renderer.initialise();
+}
