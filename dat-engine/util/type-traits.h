84d04aab411669b183133eab4f6cd1975a981783
Jtljac <jtljac2@gmail.com>
2023-07-29 01:46:43 +0100

    Organised concepts and sfinae, and added some more useful ones

--- dat-engine/util/type-traits.h
+++ dat-engine/util/type-traits.h
@@ -1,11 +1,15 @@
 //
-// Created using http://web.archive.org/web/20170705074036/https://rmf.io/cxx11/almost-static-if
+//
 //
 
 #pragma once
 
 #include <type_traits>
 #include <concepts>
+
+/* -------------------------------------------- */
+/*  Concepts                                    */
+/* -------------------------------------------- */
 
 template <typename T>
 concept integral = std::is_integral_v<T>;
@@ -16,6 +20,15 @@ concept floating_point = std::is_floating_point_v<T>;
 template <typename T>
 concept numeric = integral<T> || floating_point<T>;
 
+template <typename T, typename base>
+concept subClass = std::is_base_of_v<base, T>;
+
+template <typename T, typename exactType>
+concept exactClass = std::same_as<T, exactType>;
+
+template <typename T, typename convertsToType>
+concept convertsTo = std::convertible_to<T, convertToType>;
+
 template<typename lhType, typename rhType>
 concept comparable_to = requires(lhType lh, rhType rh) {
     {lh < rh} -> std::convertible_to<bool>;
@@ -23,6 +36,11 @@ concept comparable_to = requires(lhType lh, rhType rh) {
     {lh <= rh} -> std::convertible_to<bool>;
     {lh >= rh} -> std::convertible_to<bool>;
 };
+
+/* -------------------------------------------- */
+/*  SFINAE Shortcuts                            */
+/* -------------------------------------------- */
+// Created using http://web.archive.org/web/20170705074036/https://rmf.io/cxx11/almost-static-if
 
 namespace detail {
     /**
