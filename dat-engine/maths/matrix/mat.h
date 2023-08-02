dd36dfd474330a64b90e3a733d936c491245f4e0
Jtljac <jtljac2@gmail.com>
2023-08-02 01:22:58 +0100

    Big matrix cleanup and added copy  and identity constructors

--- dat-engine/maths/matrix/mat.h
+++ dat-engine/maths/matrix/mat.h
@@ -20,13 +20,11 @@ namespace DatMaths {
      * @tparam componentType The type of the cells in the matrix
      */
     template<int width, int height, numeric componentType>
-    struct Matrix {
-        typedef Matrix<width, height, componentType> matType;
-        typedef Matrix<height, width, componentType> transposeType;
-        typedef Vector<height, componentType> columnType;
-        typedef Vector<width, componentType> rowType;
-
-    protected:
+    class Matrix {
+        using matType = Matrix<width, height, componentType>;
+        using transposeType = Matrix<height, width, componentType>;
+        using columnType = Vector<height, componentType>;
+        using rowType = Vector<width, componentType>;
         columnType cells[width];
 
     public:
@@ -44,7 +42,7 @@ namespace DatMaths {
          * @param scalar The value to initialise the components to
          */
         Matrix(componentType scalar) {
-            std::fill_n(reinterpret_cast<componentType*>(&cells), width * height, scalar);
+            std::fill_n(reinterpret_cast<componentType*>(cells), width * height, scalar);
         }
 
         /**
@@ -58,9 +56,8 @@ namespace DatMaths {
          * Initialise each column using an array of columns
          * @param columns A list of columns to populate the matrix with
          */
-        Matrix(const std::initializer_list<columnType>& columns) {
-            assert(columns.size() == width);
-            std::memcpy(cells, columns.begin(), sizeof(columnType) * width);
+        explicit Matrix(const std::array<columnType, width>& columns) {
+            std::memcpy(cells, columns.data(), sizeof(columnType) * width);
         }
 
         /**
@@ -90,34 +87,143 @@ namespace DatMaths {
          * 2, 5, 8
          * @param cells A list of cells to populate the matrix with
          */
-        Matrix(const std::initializer_list<componentType>& cells) {
-            assert(cells.size() == width * height);
+        explicit Matrix(const std::array<componentType, width * height>& cells) {
             std::memcpy(this->cells, cells.begin(), sizeof(componentType) * width * height);
         }
 
-        // Constructor
-        //   Scalar
-        //   Vectors
-        //   Array
-        //   Raw Numbers
+        /* -------------------------------------------- */
 
-        // Identity Matrix
+        /**
+         * Initialise using values from another Matrix
+         * <br>
+         * If this Matrix is wider or taller than other Matrix then the extra cells will be initialised to zero
+         * @tparam otherWidth The width of the other Matrix
+         * @tparam otherHeight The height of the other Matrix
+         * @tparam otherComponentType The type of the other Matrix
+         * @param otherMat The other Matrix
+         */
+        template <int otherWidth, int otherHeight, numeric otherComponentType>
+        explicit Matrix(const Matrix<otherWidth, otherHeight, otherComponentType>& otherMat) {
+            for (int i = 0; i < width; ++i) {
+                for (int j = 0; j < height; ++j) {
+                    cells[i][j] = i < otherWidth && j < otherHeight ? static_cast<componentType>(otherMat[i][j]) : 0;
+                }
+            }
+        }
 
-        // Conversion
+        /* -------------------------------------------- */
+        /*  Identity                                    */
+        /* -------------------------------------------- */
+        /*
+         * These have been done individually for the main identities (mat1, mat2, mat3, mat4) as to ensure that they are
+         * optimal. The fallback system, used for mat>4, is much slower due to assignments.
+         */
+
+        /**
+         * Get a 1x1 identity matrix
+         * @return A 1x1 identity matrix
+         */
+        static matType identity()
+        requires (width == height && width == 1) {
+            return matType(static_cast<componentType>(1));
+        }
+
+        /**
+         * Get a 2x2 identity matrix
+         * @return A 2x2 identity matrix
+         */
+        static matType identity()
+        requires (width == height && width == 2) {
+            return matType(static_cast<componentType>(1), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(1));
+        }
+
+        /**
+         * Get a 3x3 identity matrix
+         * @return A 3x3 identity matrix
+         */
+        static matType identity()
+        requires (width == height && width == 3) {
+            return matType(static_cast<componentType>(1), static_cast<componentType>(0), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(1), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(0), static_cast<componentType>(1));
+        }
+
+        /**
+         * Get a 4x4 identity matrix
+         * @return A 4x4 identity matrix
+         */
+        static matType identity()
+        requires (width == height && width == 4) {
+            return matType(static_cast<componentType>(1), static_cast<componentType>(0), static_cast<componentType>(0), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(1), static_cast<componentType>(0), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(0), static_cast<componentType>(1), static_cast<componentType>(0),
+                           static_cast<componentType>(0), static_cast<componentType>(0), static_cast<componentType>(0), static_cast<componentType>(1));
+        }
+
+        /**
+         * Get the identity matrix
+         * @return An identity matrix
+         */
+        static matType identity()
+        requires (width == height && width > 4) {
+            std::array<componentType, width * height> init;
+            for (int i = 0; i < width; ++i) {
+                for (int j = 0; j < height; ++j) {
+                    if (i == j) init[i * width + j] = 1;
+                    else init[i * width + j] = 0;
+                }
+            }
+
+            return matType(init);
+        }
 
         // Assignment
 
         // Getter
-        inline columnType operator[](const size_t pos) {
+        inline columnType& operator[](const size_t pos) {
             return getColumn(pos);
         }
 
-        columnType getColumn(const size_t pos) {
+        inline columnType& operator[](const size_t pos) const {
+            return getColumn(pos);
+        }
+
+        /**
+         * Get a column of the matrix
+         * <br>
+         * This column will be a reference to the actual memory in the Matrix, therefore changes to the column will
+         * reflect in the matrix.
+         * @param pos The index of the column to get
+         * @return The column at the given position
+         */
+        columnType& getColumn(const size_t pos) {
             assert(pos < width);
             return cells[pos];
         }
 
-        rowType getRow(const size_t pos) {
+        /**
+         * Get a column of the matrix
+         * <br>
+         * This column will be a reference to the actual memory in the Matrix, therefore changes to the Matrix will
+         * reflect on the column.
+         * @param pos The index of the column to get
+         * @return The column at the given position
+         */
+        columnType& getColumn(const size_t pos) const {
+            assert(pos < width);
+            return cells[pos];
+        }
+
+        /**
+         * Get a row of the matrix
+         * <br>
+         * This operation is more expensive than getColumn() as it needs to create a new vector. Subsequently, the returned
+         * row is not a reference and changes to the row will not affect the Matrix.
+         * @param pos
+         * @return
+         */
+        rowType getRow(const size_t pos) const {
             assert(pos < height);
             rowType row;
             for (int i = 0; i < width; ++i) {
